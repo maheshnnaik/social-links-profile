f318ef0d34365a49af6597d1fb3f4a1388f5db1b
maheshnnaik <maheshnnaik07@gmail.com>
2024-07-01 22:30:33 +0530

    Add avatar image to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SocialProfile } from './SocialProfile';
 
 function App() {
   return (
     <div className="App">
+      <SocialProfile />
     </div>
   );
 }
---
+++ src/SocialProfile.tsx
@@ -0,0 +1,9 @@
+export const SocialProfile = () => {
+	return (
+		<div className="container">
+			<div className="avatar">
+				<img src="./images/avatar.jpeg" alt="Profile" />
+			</div>
+		</div>
+	)
+}
